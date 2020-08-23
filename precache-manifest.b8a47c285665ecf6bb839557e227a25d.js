c4626847de56f284ffa7876d0e0c81bb9fed89e0
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-08-23 12:54:21 -0500

    Updates

---
+++ precache-manifest.b8a47c285665ecf6bb839557e227a25d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6c3a495ee8dcdf579c2bb3305c9de8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4467cf6380c8658bab95",
+    "url": "/react-portfolio/static/css/main.feaf694c.chunk.css"
+  },
+  {
+    "revision": "01e454dbd2ae7739fca5",
+    "url": "/react-portfolio/static/js/2.e2b8c35d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.e2b8c35d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4467cf6380c8658bab95",
+    "url": "/react-portfolio/static/js/main.62226178.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "656e540533f536cb1d05267c0c7cca04",
+    "url": "/react-portfolio/static/media/Alan2.656e5405.jpg"
+  },
+  {
+    "revision": "17df57d9f09383f076c949b0194b919f",
+    "url": "/react-portfolio/static/media/background.17df57d9.jpg"
+  }
+]);
