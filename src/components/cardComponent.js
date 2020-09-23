42eba10b656277b03cf6093a1d6ba5b6056ed351
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-09-23 13:51:42 -0500

    added images

--- src/components/cardComponent.js
+++ src/components/cardComponent.js
@@ -3,16 +3,17 @@ import Image from "../Images/background.jpg";
 import "../styles/portfolio.css";
 
 function CardBody(props) {
+    console.log(props);
     return(
         props.projects.map(project => {
             return(
                 <div className="col-lg-4 col-md-6 col-sm-6 col-6 box">
                     <div className="card">
-                        <a href={project.url} target="_blank" rel="noopener noreferrer"><img src={Image} className="card-img-top img-container" alt="..."></img></a>
+                        <a href={project.url} target="_blank" rel="noopener noreferrer"><img src={project.image} className="card-img-top card-img-top-2 img-container" alt="..."></img></a>
                         <div className="card-body">
                             <h5 className="card-title" key={props.projects.id}>{project.projectName}</h5>
-                            <button type="button" class="btn btn-primary" data-toggle="modal" onClick={props.handleBtnClick} data-value={project.id} data-target="#staticBackdrop">
-                                See Description
+                            <button type="button" className="btn btn-primary" data-toggle="modal" onClick={props.handleBtnClick} data-value={project.id} data-target="#staticBackdrop">
+                                Details
                             </button>
                         </div>
                     </div>
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,30 +1,33 @@
 import React from "react";
 import { Modal, Button } from 'react-bootstrap';
+import "../styles/modal.css"
 
 function ModalProject (props) {
-    console.log(props.project);
+    console.log(props.project.map(item => item.projectName));
+    
     return (
         props.project.map(project => {
             return (
                 <Modal
         {...props}
-        size="lg"
+        size="md"
         aria-labelledby="contained-modal-title-vcenter"
         centered
         >
-      <Modal.Header closeButton>
+      <Modal.Header closeButton className="modal-header">
         <Modal.Title id="contained-modal-title-vcenter">
-          {project.projectName}
+          <h5 className="project-title">{project.projectName}</h5>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>
+        <p className="project-description">
           {project.projectDescription}
         </p>
       </Modal.Body>
-      <Modal.Footer>
+      <Modal.Footer className="modal-header">
         <Button onClick={props.onHide}>Close</Button>
-        <a href={project.url} target="_blank" rel="noreferrer noopener"><Button>Go!</Button></a>
+        <a href={project.github} target="_blank" rel="noreferrer noopener"><Button>GitHub Repo!</Button></a>
+        <a href={project.url} target="_blank" rel="noreferrer noopener"><Button>Live Demo!</Button></a>
       </Modal.Footer>
     </Modal>
             )
